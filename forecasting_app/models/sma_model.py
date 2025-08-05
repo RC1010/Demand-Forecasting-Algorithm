a1fdd8a7bad386c5fa5d880e04a8b535af52576e
RC1010 <91231779+RC1010@users.noreply.github.com>
2025-08-05 21:26:43 +0800

    Delete forecasting_app/models directory

--- forecasting_app/models/sma_model.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-def calculate_sma_forecast(data, window_size=7):
-    data = data.copy()
-    data['SMA_Prediction'] = data['Quantity Ordered'].rolling(window=window_size).mean().shift(1)
-    return data
